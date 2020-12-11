b6c71a8eae280c2e1d9ed6c74db165626c645d65
Valentin Alberto <valalberto0214@gmail.com>
2020-12-11 18:10:03 +0800

    remove unused import packages

--- src/main/java/com/jlib/via/ams/app/repository/AmsRepositoryImpl.java
+++ src/main/java/com/jlib/via/ams/app/repository/AmsRepositoryImpl.java
@@ -1,11 +1,9 @@
 package com.jlib.via.ams.app.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
@@ -15,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.jlib.via.ams.app.domain.Student;
-import com.jlib.via.ams.app.service.AmsServiceImpl;
 
 /**
  * @author albertv
