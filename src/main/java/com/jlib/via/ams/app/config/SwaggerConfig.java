b95639ae3171d2165222d7f3e55436ddcf44c85a
Valentin Alberto <valalberto0214@gmail.com>
2020-12-11 17:59:50 +0800

    remove basic error controller section in swagger

--- src/main/java/com/jlib/via/ams/app/config/SwaggerConfig.java
+++ src/main/java/com/jlib/via/ams/app/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.jlib.via.ams.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -22,8 +23,8 @@ public class SwaggerConfig {
 	public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
+          .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))              
+          .paths(PathSelectors.any())
           .build();                                           
     }
 
